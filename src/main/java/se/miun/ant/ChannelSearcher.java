43901cacd9dfc0bf85f39185decfbc6ec4eedbb7
Kalle Lindström <lindstrom.kalle@gmail.com>
2013-07-31 20:20:03 +0200

    Refactored class and added javadoc. Implemented check to see if search is already in progress. Added search termination if a channel times out.

--- src/main/java/se/miun/ant/ChannelSearcher.java
+++ src/main/java/se/miun/ant/ChannelSearcher.java
@@ -1,6 +1,6 @@
 package se.miun.ant;
 
-import android.content.Context;
+import android.app.Activity;
 import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
@@ -8,118 +8,218 @@ import android.widget.Toast;
 import com.dsi.ant.channel.AntChannel;
 import com.dsi.ant.channel.AntCommandFailedException;
 import com.dsi.ant.channel.IAntChannelEventHandler;
+import com.dsi.ant.message.EventCode;
+import com.dsi.ant.message.fromant.ChannelEventMessage;
 import com.dsi.ant.message.fromant.MessageFromAntType;
 import com.dsi.ant.message.ipc.AntMessageParcel;
 
-
+/**
+ * ChannelSearcher is used to open ANT channels that will try to connect to an available
+ * master channel. If the connection is made, the AntChannel object is passed to an observer.
+ * A search can be initiated by calling startChannelSearch() on an instance of this class.
+ * The search will continue to create and connect (sequentially) new ANT channels until
+ * a channel times out and then it will stop.
+ */
 public class ChannelSearcher implements ChannelRetriever.OnChannelProviderAvailableListener {
 
+    /**
+     * The OnChannelConnectedListener must be implemented by a client (client to this class)
+     * that wishes to be notified when an ANT channel has been successfully created, opened and
+     * connected.
+     */
     public interface OnChannelConnectedListener {
+        /**
+         * Called when the channel has completed its initialization and it is safe to call
+         * startChannelSearch().
+         */
         public void onChannelSearcherInitialized();
+
+        /**
+         * Called when an ANT channel has been successfully created, opened and connected.
+         * @param antChannel the connected ANT channel.
+         */
         public void onChannelConnected(AntChannel antChannel);
-    }
-
-    private class EventHandler implements IAntChannelEventHandler {
-
-        private AntChannel channel;
-
-        public EventHandler(AntChannel channel) {
-            this.channel = channel;
-        }
-
-        @Override
-        public void onReceiveMessage(MessageFromAntType messageType, AntMessageParcel messageParcel) {
-
-            if (messageType == MessageFromAntType.BROADCAST_DATA) {
-                try {
-                    channel.clearChannelEventHandler();
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-
-                listener.onChannelConnected(channel);
-
-                startChannelSearch();
-            }
-
-        }
-
-        @Override
-        public void onChannelDeath() {}
     }
 
     public static final String TAG = "ANTLightController";
 
-    private static int currentChannelId = 1;
+    private Activity activity;                      // Activity used for Toast.
+    private OnChannelConnectedListener listener;    // Is notified when a channel has connected.
+    private ChannelRetriever channelRetriever;      // Used to get channels from the ANT system.
+    private ChannelInitializer channelInitializer;  // Used to set default channel parameters.
 
-    private Context context;
-    private OnChannelConnectedListener listener;
-    private ChannelRetriever channelRetriever;
-    private ChannelInitializer channelInitializer;
+    // Set to true when a search is in progress, otherwise set to false.
+    // A search is in progress from the moment a channel search is initialized until there
+    // is a channel timeout (meaning no master channel to connect to) on one of the opened channels.
+    private boolean searchInProgress;
 
-    public ChannelSearcher(Context context) {
-        this.context       = context;
-        channelRetriever   = new ChannelRetriever(context, this);
+    /**
+     * Constructor.
+     * @param activity the Activity that is needed for Toast notifications.
+     */
+    public ChannelSearcher(Activity activity) {
+        this.activity      = activity;
+        channelRetriever   = new ChannelRetriever(activity, this);
         channelInitializer = new ChannelInitializer();
+        searchInProgress   = false;
     }
 
+    /**
+     * Constructor.
+     * @param activity the Activity that is needed for Toast notifications.
+     * @param listener this object will be notified when an ANT slave channel has been
+     *                 successfully created, opened and connected.
+     */
+    public ChannelSearcher(Activity activity, OnChannelConnectedListener listener) {
+        this(activity);
+        setOnChannelConnectedListener(listener);
+    }
+
+    /**
+     * This callback is called when the ANT Radio Service has finished initializing and
+     * the channel provider can be used to obtain ANT channels from the service.
+     */
     @Override
     public void onChannelProviderAvailable() {
         if (listener != null) listener.onChannelSearcherInitialized();
     }
 
-    public void startChannelSearch() {
-
-        AntChannel channel = null;
-
-        try {
-            channel = channelRetriever.getChannel();
-            channelInitializer.initializeChannel(channel, getNewChannelId());
-        } catch (ChannelRetriever.ChannelRetrieveException e) {
-            Log.e(TAG, "Unable to retrieve channel: " + e.getMessage());
-            notifyUserChannelError(e);
-            return;
-        } catch (ChannelInitializer.ChannelInitializationException e) {
-            Log.e(TAG, "Unable to initialize channel: " + e.getMessage());
-            notifyUserChannelError(e);
-            return;
-        }
-
-        try {
-            channel.setChannelEventHandler(new EventHandler(channel));
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-
-
-        tryOpenChannel(channel);
-    }
-
-    public void addOnChannelConnectedListener(OnChannelConnectedListener listener) {
+    /**
+     * Sets the OnChannelConnectedListener that is notified when a channel has connected.
+     * @param listener the listener that will be notified.
+     */
+    public void setOnChannelConnectedListener(OnChannelConnectedListener listener) {
         this.listener = listener;
     }
 
-    private int getNewChannelId() {
-        int id = currentChannelId;
-        currentChannelId += 1;
-        return id;
+    /**
+     * This method requests that a channel search should be started. If a channel search is
+     * already in progress and this method is called it will not do anything.
+     */
+    public void startChannelSearch() {
+        if (!searchInProgress) {
+            startChannelSearchThread();
+            searchInProgress = true;
+        }
+    }
+
+    // Starts the thread that runs a channel search. The thread will retrieve, initialize and open
+    // one ANT channel and then exit.
+    private void startChannelSearchThread() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                AntChannel channel;
+
+                try {
+                    channel = channelRetriever.getChannel();
+                    channelInitializer.initializeChannel(channel);
+                    channel.setChannelEventHandler(new AntChannelEventHandler(channel));
+
+                } catch (ChannelRetriever.ChannelRetrieveException e) {
+                    logErrorAndNotify("Unable to retrieve channel: " + e.getMessage(), e);
+                    return;
+                } catch (ChannelInitializer.ChannelInitializationException e) {
+                    logErrorAndNotify("Unable to initialize channel: " + e.getMessage(), e);
+                    return;
+                } catch (RemoteException e) {
+                    logErrorAndNotify("Unable to initialize channel: " + e.getMessage(), e);
+                    return;
+                }
+
+                tryOpenChannel(channel);
+            }
+        }).start();
     }
 
     private void tryOpenChannel(AntChannel channel) {
         try {
             channel.open();
         } catch (RemoteException e) {
-            Log.e(TAG, "Unable to open channel: " + e.getMessage());
-            notifyUserChannelError(e);
+            logErrorAndNotify("Unable to open channel: " + e.getMessage(), e);
         } catch (AntCommandFailedException e) {
-            Log.e(TAG, "Unable to open channel: " + e.getMessage());
-            notifyUserChannelError(e);
+            logErrorAndNotify("Unable to open channel: " + e.getMessage(), e);
         }
     }
 
-    private void notifyUserChannelError(Exception e) {
-        Toast.makeText(context,
-                       "Error opening ANT channel: " + e.getMessage() + "\nPlease try again.",
-                       Toast.LENGTH_LONG).show();
+    private void logErrorAndNotify(String message, Exception e) {
+        Log.e(TAG, message);
+        notifyUserChannelError(e);
+    }
+
+    private void notifyUserChannelError(final Exception e) {
+        activity.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(activity,
+                        "Error opening ANT channel: " + e.getMessage() + "\nPlease try again.",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /**
+     * This class is used to listen for data received on a newly opened channel in order
+     * to know whether the channel connected successfully or not.
+     */
+    private class AntChannelEventHandler implements IAntChannelEventHandler {
+
+        private AntChannel channel;
+
+        /**
+         * Constructor.
+         * @param channel the ANT channel that this object will listen for messages from.
+         */
+        public AntChannelEventHandler(AntChannel channel) {
+            this.channel = channel;
+        }
+
+        /**
+         * Checks if a message is received and if it is a broadcast data message. If so, the
+         * channel has successfully connected, and a new channel search is started.
+         * @param messageType the ANT message type.
+         * @param messageParcel used to construct an object that can read the message contents.
+         */
+        // TODO: cleanup
+        @Override
+        public void onReceiveMessage(MessageFromAntType messageType, AntMessageParcel messageParcel) {
+
+            if (messageType == MessageFromAntType.BROADCAST_DATA) {
+
+                try {
+                    channel.clearChannelEventHandler();
+                } catch (RemoteException e) {
+                    Log.e(TAG, "Unable to clear ANT channel event handler: " + e.getMessage());
+                }
+
+                listener.onChannelConnected(channel);
+                startChannelSearchThread();
+            } else if (messageType == MessageFromAntType.CHANNEL_EVENT) {
+
+                ChannelEventMessage eventMessage = new ChannelEventMessage(messageParcel);
+
+                if (eventMessage.getEventCode() == EventCode.RX_SEARCH_TIMEOUT) {
+                    Log.i(TAG, "Channel timeout");
+
+                    channel.release();
+                    searchInProgress = false;
+                }
+            }
+        }
+
+        // TODO: This is a debug implementation. Change later.
+        @Override public void onChannelDeath() {
+            activity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    Toast.makeText(activity,
+                                   "onChannelDeath() called",
+                                   Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
